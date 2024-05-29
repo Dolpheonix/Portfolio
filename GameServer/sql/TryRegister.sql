69ef967d344512ce5f58baa9a0972c127f98dfcd
Dolpheonix <moonfog97@gmail.com>
2024-05-29 16:20:11 +0900

    ee

--- GameServer/sql/TryRegister.sql
+++ GameServer/sql/TryRegister.sql
@@ -3,19 +3,16 @@ BEGIN
     DECLARE userIndex INT DEFAULT NULL;
     DECLARE cnt INT DEFAULT 0;
 
-    -- Check if user with given ID exists
     SELECT COUNT(*) INTO cnt
     FROM userinfo
     WHERE userinfo.id = ID;
 
-    -- If no user exists, insert new record
     IF cnt = 0 THEN
         INSERT INTO userinfo(id, pw)
         VALUES (ID, PW);
 
         SELECT LAST_INSERT_ID() AS userIdx;
     ELSE
-        -- If user exists, get userIdx and return -1
         SELECT userinfo.userIdx 
         INTO userIndex
         FROM userinfo
